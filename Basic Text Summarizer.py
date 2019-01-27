9ccee46c572f539f7d259ae3e476a77b816f47cf
Pradip Lamsal <pradip.lamsal@ttu.edu>
2019-01-27 14:09:05 -0600

    most updated one from TAMU Hackathon

--- Basic Text Summarizer.py
+++ Basic Text Summarizer.py
@@ -1,33 +1,40 @@
 import requests
 from pprint import pprint
 
-subscription_key = "14b8a2715f434596b70e32d6d961b16c"
+subscription_key = "89ee964bcbc64e61a26e16cf091840de"
 assert subscription_key
 
 text_analytics_base_url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/"
 key_phrase_api_url = text_analytics_base_url + "keyPhrases"
 
 print("Please paste the text to summarize here: \n")
-text = input()
-##text = input().split(".")
-##ct = 1
-##documents = {'documents' : []}
-##for i in text:
-##    c = str(ct)
-##    dictt = {'id': c, 'text': i}
-##    documents['documents'].append(dictt)
-##    ct+=1
-##print(documents)
-documents = {'documents' : [
-  {'id': '1', 'text': text}
-]}
+text = input().split(".")
+ct = 1
+documents = {'documents' : []}
+
+for i in text:
+    c = str(ct)
+    dictt = {'id': c, 'text': i}
+    documents['documents'].append(dictt)
+    ct+=1
+
 headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
 response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
 key_phrases = response.json()
-ls = key_phrases['documents'][0]['keyPhrases']
-result = ""
+
+docs = key_phrases['documents']
+ls = []
+resultt = ""
+for i in docs:
+    result = ""
+    for j in i['keyPhrases']:
+        result += " " + j
+    result += "."
+    ls.append(result)
+    
 print("Here is the summary with the key phrases: \n")
+
 for i in ls:
-    result += i
-result += "."
-print('"' + result + '"')
+    resultt += i
+    
+print('"' + resultt + '"')
